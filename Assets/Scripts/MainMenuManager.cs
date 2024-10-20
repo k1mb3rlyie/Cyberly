5acccd030b36cb68fe1732981efb06dee454a329
Kimberly Dawap <kimberlytipand@gmail.com>
2024-10-20 21:24:04 +0100

    some UI changes

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -17,6 +17,7 @@ public class MainMenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MainMenuPanel.SetActive(true);
         SettingsPanel.SetActive(false);
         AchievementsPnl.SetActive(false);
         User.SetActive(false);
